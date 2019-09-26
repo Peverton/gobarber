eb49804b59e4dd61a9500e1b5e00f0858be1b549
Peverton <pedro.ev14@hotmail.com>
2019-09-25 23:26:27 -0300

    Sending email with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0f762b447c7069',
+    pass: 'f5dfc71d2aa46d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
